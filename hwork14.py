9b2b1d9642881ade3edfb615fdbc54322fded673
smilina702 <32021201+smilina702@users.noreply.github.com>
2018-06-20 17:22:00 +0300

    Add files via upload

---
+++ hwork14.py
@@ -0,0 +1,14 @@
+def read_text():
+    elements = '.?!'
+    text = []
+    with open("hemi.txt", "r", encoding = "utf-8") as file:
+        for lines in file:
+            lines = lines.split(elements)
+            text.append(lines)
+    return text
+
+def main():
+    text = read_text()
+    print(text)
+    
+main()
